287c986677fd78fdad43a69b706ba528b783f203
Justin Rouzzo <jcrouzzo@gmail.com>
2021-09-04 13:05:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App bg-white">
       <header className="App-header container bg-danger text-light">
-        <h3 className = 'display-3 '>Pommeranian Pom Timer</h3>
+        <h3 className = 'display-3 '>PomPom Time Manager</h3>
       </header>
       <div className="container">
         <Pomodoro />
